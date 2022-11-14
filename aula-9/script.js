7a3ac1cce2034c6e491bc8ef7766283698230cdc
Edelson Barboza <edelsonbsilva@gmail.com>
2022-11-14 20:25:54 -0300

    Função Parte III

---
+++ aula-9/script.js
@@ -0,0 +1,9 @@
+// Função anônima
+
+(function () {
+    console.log('PlayLivre S2')
+})()// Autoexecução
+
+setTimeout(function(){
+    console.log('Vou levar 3 segundos para ser executado!')
+},3000)// 3s
