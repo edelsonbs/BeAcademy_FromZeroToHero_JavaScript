58ccca8b10699324007f6f1d74f9201406f9c736
Edelson Barboza <edelsonbsilva@gmail.com>
2022-11-14 20:52:44 -0300

    Função Parte IV

---
+++ aula-10/script.js
@@ -0,0 +1,47 @@
+/* Parte 1 */
+
+// let assunto
+
+// function criarAssunto(assunto) {
+//     return assunto
+// }
+
+// console.log(criarAssunto(assunto))
+
+
+/* Parte 2 */
+
+// let assunto = 'Estudar' // var = valor
+
+// function criarAssunto(assunto) {
+//     return assunto //
+// }
+
+// console.log(assunto) // resposta => 'Estudar'
+// console.log(criarAssunto(assunto)) // resposta => 'Estudar'
+
+
+/* Parte 3 */
+
+// let assunto = 'Estudar' // var = valor
+
+// function criarAssunto(assunto) {
+//     assunto = "Escutar música" // mudança de valor
+//     return assunto //
+// }
+
+// console.log(assunto) // resposta => 'Estudar'
+// console.log(criarAssunto(assunto)) // resposta => 'Escutar música'
+
+
+/* Parte 4 */
+
+let assunto = 'Estudar' // var = valor
+
+function criarAssunto() { // sem parâmetro 
+    assunto = "Escutar música" // mudança de valor
+    return assunto //
+}
+
+console.log(assunto) // resposta => 'Estudar'
+console.log(criarAssunto(assunto)) // resposta => 'Escutar música'
